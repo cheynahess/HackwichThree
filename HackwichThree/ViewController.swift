7792748ddd8bfa7dbd48cb316010867d10425219
Cheyna Hess <cheynah@hawaii.edu>
2022-02-10 11:41:06 -1000

    Completed Parts 1-3

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var firstString = "The background color will turn blue"
+    var secondString = "The background color will turn green"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func ChangeColorButtonPressed(_ sender: Any) {
+        if firstString == "The background color will turn red"
+        {
+            //since conditional statement evaluates to false, it will skip the code in the if block
+            self.view.backgroundColor = UIColor.red
+            
+        }else{
+            
+            self.view.backgroundColor = UIColor.blue
+        }
+}
 }
 
